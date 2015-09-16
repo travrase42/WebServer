c435f5e26327d13c232ca0ce056b58470f684892
travrase42 <travrase42@gmail.com>
2015-09-16 15:01:38 -0600

    everything working, with two JavaScripts

--- static/java.js
+++ static/java.js
@@ -1,5 +1,11 @@
 
 function changeImage() {
     var image = document.getElementById('rainbowTrout');
-    image.src = "static/steelhead.jpg"
+    image.src = "static/steelhead.jpg";
     }
+function bestFly() {
+    var text = document.getElementById('info');
+    var image = document.getElementById('mysteryPic');
+    text.innerHTML = "Elk Hair Caddis";
+    image.src = "static/elkHair.jpg";
+  }
